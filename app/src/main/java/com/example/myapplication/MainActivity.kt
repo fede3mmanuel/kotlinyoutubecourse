05c38ec4cb7d383bf9685648d69ababbf987e3a6
Emmanuel Sanchez <fede@fede.website>
2022-11-17 17:41:15 -0600

    changing the visibility of the butto with an if

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,17 +16,20 @@ class MainActivity : AppCompatActivity() {
             val greetingTextView = findViewById<TextView>(R.id.tvHello)
             val inputField = findViewById<EditText>(R.id.etName)
             val submitButton = findViewById<Button>(R.id.btnSubmit)
+            val offersButton = findViewById<Button>(R.id.btnOffers)
 
             submitButton.setOnClickListener {
                 val enteredName = inputField.text.toString()
 
                 if (enteredName == ""){
+                    offersButton.visibility = INVISIBLE
                     greetingTextView.text = ""
                     Toast.makeText(this@MainActivity, "Please, enter your name!", Toast.LENGTH_SHORT ).show()
                 }else {
                     val message = "Welcome ${enteredName}"
                     greetingTextView.text = message
                     inputField.text.clear()
+                    offersButton.visibility = VISIBLE
                 }
             }
     }
