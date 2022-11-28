0e6175a7b9b7a068aa63149c555b8368676084b2
Emmanuel Sanchez <fede@fede.website>
2022-11-28 14:04:06 -0600

    added log cat

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("MyTag", "MainActivity: OnCreate")
             val greetingTextView = findViewById<TextView>(R.id.tvHello)
             val inputField = findViewById<EditText>(R.id.etName)
             val submitButton = findViewById<Button>(R.id.btnSubmit)
@@ -30,7 +32,9 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this@MainActivity, "Please, enter your name!", Toast.LENGTH_SHORT ).show()
                 }else {
                     val message = "Welcome ${enteredName}"
+                    Log.i("MyTag", message)
                     greetingTextView.text = message
+                    Log.i("MyTag", "After displaying the message on the Text View ")
                     inputField.text.clear()
                     offersButton.visibility = VISIBLE
                 }
@@ -41,5 +45,9 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("USER", enteredName)
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }
